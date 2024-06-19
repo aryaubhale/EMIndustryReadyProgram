acee7b53daa3d136b78f95a0c06d2680a9fa434a
aryaubhale <aryaubhale30@gmail.com>
2024-06-19 17:00:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Toggle nav menu on mobile
+const navMenu = document.querySelector('.nav-menu');
+document.querySelector('.logo').addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+navMenu.querySelectorAll('li').forEach(item => {
+    item.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+    });
+});
